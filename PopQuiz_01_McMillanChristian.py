ff7d3ef40911be07b489d186e95d3a8824abd31c
meekmillan <31966468+meekmillan@users.noreply.github.com>
2017-09-26 12:32:29 -0400

    Add files via upload

---
+++ PopQuiz_01_McMillanChristian.py
@@ -0,0 +1,15 @@
+# CTI-110
+# PopQuiz_01
+# Christian McMillan
+# 9/26
+# This program will print the average of two numbers
+
+#Input values
+value_a = float(input('Enter value a: '))
+value_b = float(input('Enter value a: '))
+
+#Find average
+average = (value_a + value_b) / 2
+
+#Display average
+print('The average is', format(average))
